9a3f4ccd12976e8b56966c398fc77c1d047b50ae
Qinghuatang <1822064204@qq.com>
2025-01-19 22:11:56 +0800

    添加店铺类型缓存功能的代码实现

--- src/main/java/com/hmdp/controller/ShopTypeController.java
+++ src/main/java/com/hmdp/controller/ShopTypeController.java
@@ -11,24 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
-/**
- * <p>
- * 前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
- */
 @RestController
 @RequestMapping("/shop-type")
 public class ShopTypeController {
     @Resource
     private IShopTypeService typeService;
 
+    /**
+     * 查询店铺类型
+     * @return
+     */
     @GetMapping("list")
     public Result queryTypeList() {
-        List<ShopType> typeList = typeService
-                .query().orderByAsc("sort").list();
-        return Result.ok(typeList);
+        //List<ShopType> typeList = typeService.query().orderByAsc("sort").list();
+        //return Result.ok(typeList);
+
+        // 上面是原代码
+        return typeService.queryTypeList();
+
     }
 }
