e12fecce0c1cd19aa84b40267a2597789d76ec4a
Qinghuatang <1822064204@qq.com>
2025-01-19 15:39:55 +0800

    短信验证码登录和注册功能的代码实现

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.RegexUtils;
+import com.hmdp.utils.SystemConstants;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,13 @@ import javax.servlet.http.HttpSession;
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送验证码
+     *
+     * @param phone
+     * @param session
+     * @return
+     */
     @Override
     public Result sendCode(String phone, HttpSession session) {
 
@@ -36,5 +45,54 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 返回ok
         return Result.ok();
+    }
+
+    /**
+     * 用户登录
+     *
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误！");
+        }
+
+        // 2.校验验证码
+        String cacheCode = (String) session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cacheCode == null || !cacheCode.equals(code)){
+            // 3.验证码不一致，返回错误信息
+            return Result.fail("验证码错误！");
+        }
+
+        // 4.验证码一致，根据手机号查询用户 select * from tb_user where phone = ?
+        User user = query().eq("phone", phone).one();   // 使用到了 MyBatis Plus 的功能
+
+        // 5.判断用户是否存在
+        if (user == null){
+            // 6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        // 7.保存用户信息到session中
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1.创建用户
+        User user = new User();
+        user.setNickName(SystemConstants.USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        user.setPhone(phone);
+
+        // 2.保存用户
+        save(user);
+
+        return user;
     }
 }
