c90f86770aee42f73b34a30470d1c7625b154f9c
Qinghuatang <1822064204@qq.com>
2025-01-20 12:06:13 +0800

    利用互斥锁解决缓存击穿问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,5 +1,7 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.hmdp.dto.Result;
@@ -34,7 +36,18 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryShopById(Long id) {
+        // 缓存穿透
+        // Shop shop = queryWithPassThrough(id);
 
+        // 互斥锁解决缓存击穿
+        Shop shop = queryWithMutex(id);
+        if (shop == null) {
+            return Result.fail("店铺不存在！");
+        }
+        return Result.ok(shop);
+    }
+
+    public Shop queryWithPassThrough(Long id){  // 缓存穿透
         String key = CACHE_SHOP_KEY + id;
 
         // 1.从redis中查询商铺缓存
@@ -43,13 +56,13 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 2.若redis缓存中存在商铺信息，则直接返回
         if (StrUtil.isNotBlank(shopJson)) {
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
-            return Result.ok(shop);
+            return shop;
         }
 
         // 判断命中的是否是空值
         if(shopJson != null){   // 命中的是空字符串
             // 返回一个错误信息
-            return Result.fail("店铺信息不存在！");
+            return null;
         }
 
         // 3.若redis缓存中不存在商铺信息，则在数据库中查询商铺信息，并添加到redis缓存中
@@ -60,15 +73,83 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
             // 将空值写入redis
             stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
             // 返回错误信息
-            return Result.fail("店铺不存在！");
+            return null;
         }
 
         // 5.存在，写入redis
         stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         // 6.返回
-        return Result.ok(shop);
+        return shop;
     }
+
+    public Shop queryWithMutex(Long id){  // 互斥锁解决缓存击穿
+        String key = CACHE_SHOP_KEY + id;
+
+        // 1.从redis中查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        // 2.若redis缓存中存在商铺信息，则直接返回
+        if (StrUtil.isNotBlank(shopJson)) {
+            return JSONUtil.toBean(shopJson, Shop.class);
+        }
+
+        // 判断命中的是否是空值
+        if(shopJson != null){   // 命中的是空字符串[穿透的结果]
+            // 返回一个错误信息
+            return null;
+        }
+        // 3.实现缓存重建
+        // 3.1 获取互斥锁
+        String lockKey = LOCK_SHOP_KEY + id;
+
+        Shop shop = null;
+        try {
+            boolean isLock = tryLock(lockKey);
+            // 3.2 判断是否获取成功
+            if(!isLock){
+                // 3.3 失败，则休眠并重试
+                Thread.sleep(50);
+                return queryWithMutex(id);
+            }
+
+            // 3.4 成功，在数据库中查询商铺信息
+            shop = getById(id); // 调用 MyBatis Plus 的api
+            // 模拟重建的延时
+            Thread.sleep(200);
+
+            // 4.不存在，返回错误
+            if (shop == null) {
+                // 将空值写入redis
+                stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+                // 返回错误信息
+                return null;
+            }
+
+            // 5.存在，写入redis
+            stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            // 6.释放互斥锁
+            unLock(lockKey);
+        }
+
+        // 7.返回
+        return shop;
+    }
+
+    private boolean tryLock(String key){
+        // 使用redis的 setnx 方式实现互斥锁
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unLock(String key){
+        // 释放互斥锁
+        stringRedisTemplate.delete(key);
+    }
+
 
     @Override
     @Transactional
